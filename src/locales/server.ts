58e636319218ce2564a36e89e889095e63ea4c7c
soheil mansoori <ahadmansoori369@gmail.com>
2025-07-27 21:08:56 +0330

    delete prev next-intl config file

--- src/locales/server.ts
+++
@@ -1,7 +0,0 @@
-import { createI18nServer } from 'next-international/server';
-// import en from './en';
-
-export const { getI18n, getScopedI18n, getCurrentLocale, getStaticParams } = createI18nServer({
- en: () => import('./en'),
- fa: () => import('./fa'),
-});
