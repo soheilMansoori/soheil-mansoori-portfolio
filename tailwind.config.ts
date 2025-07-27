68fb017f50a01931aeaed1938a31c8e772e79a1a
soheil mansoori <ahadmansoori369@gmail.com>
2025-07-27 21:04:25 +0330

    fix tailwnd file for track tsx files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        vazir: ["Vazirmatn", "sans-serif"],
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        whiteBg: "#e8e8e8",
-        blackBg: "#08080f",
-        purpl: "rgb(103 61 181)"
-      },
-    },
+ content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+ theme: {
+  extend: {
+   fontFamily: {
+    vazir: ['Vazirmatn', 'sans-serif'],
+   },
+   colors: {
+    background: 'var(--background)',
+    foreground: 'var(--foreground)',
+    whiteBg: '#e8e8e8',
+    blackBg: '#08080f',
+    purpl: 'rgb(103 61 181)',
+   },
   },
-  plugins: [],
+ },
+ plugins: [],
 } satisfies Config;
